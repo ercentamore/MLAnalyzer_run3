2d3c2f64d168c1eb0cedc2b011a8c6cad97b39dc
ercentamore <ercentamore@crimson.ua.edu>
2025-09-26 17:02:09 -0700

    Update runRHAnalyzer.py

--- runRHAnalyzer.py
+++ runRHAnalyzer.py
@@ -2,7 +2,7 @@ import os
 
 #cfg='RecHitAnalyzer/python/ConfFile_data_cfg.py'
 cfg='RecHitAnalyzer/python/ConfFile_cfg.py'
-inputFiles_='file:/uscms/home/ecentamo/nobackup/CMSSW_13_0_13/src/nobackup/CMSSW_13_0_13/src/MCProduction/E2E-HToAATo2Tau2Photon/NanoAOD_HToAATo2Gluon2Photon.root'#pixel checks
+inputFiles_='file:/uscms/home/ecentamo/nobackup/CMSSW_13_0_13/src/MCProduction/E2E-HToAATo2Tau2Photon/NanoAOD_HToAATo2Gluon2Photon.root'#pixel checks
 # inputFiles_='file:step3_AODSIM_M14_1.root'
 
 maxEvents_=-1
