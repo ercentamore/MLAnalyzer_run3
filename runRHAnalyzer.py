b29fc9c507c5df60f982b16e27c77698fa959ff0
ercentamore <ercentamore@crimson.ua.edu>
2025-11-17 09:24:38 -0600

    Update runRHAnalyzer.py

--- runRHAnalyzer.py
+++ runRHAnalyzer.py
@@ -2,13 +2,13 @@ import os
 
 #cfg='RecHitAnalyzer/python/ConfFile_data_cfg.py'
 cfg='RecHitAnalyzer/python/ConfFile_cfg.py'
-inputFiles_= 'file:/uscms/home/ecentamo/nobackup/CMSSW_13_0_13/src/MCProduction/E2E-HToAATo2Tau2Photon/MiniAOD_file_list.txt' #pixel checks
+inputFiles_= 'file://uscms/home/ecentamo/nobackup/CMSSW_13_0_13/src/MCProduction/E2E-HToAATo2Tau2Photon/AOD_HToAATo2Gluon2Photon_extra_collection.root' #pixel checks
 # inputFiles_='file:step3_AODSIM_M14_1.root'
 
 maxEvents_=-1
 skipEvents_=0#
 # outputFile_='MLAnalyzer_HToAATo2Tau2Photon_with_triggers.root'
-outputFile_='MLAnalyzer_HToAATo2Gluon2Photon.root'
+outputFile_='MLAnalyzer_HToAATo2Gluon2Photon_11_17.root'
 
 # cmd="cmsTraceExceptions cmsRun %s inputFiles=%s maxEvents=%d skipEvents=%d outputFile=%s"%(cfg,inputFiles_,maxEvents_,skipEvents_,outputFile_)
 cmd="cmsRun %s inputFiles=%s maxEvents=%d skipEvents=%d outputFile=%s"%(cfg,inputFiles_,maxEvents_,skipEvents_,outputFile_)
