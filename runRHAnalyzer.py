417059ed3bbfc1e1812f6ebe98da51e6c39357b0
ercentamore <ercentamore@crimson.ua.edu>
2025-11-17 19:33:57 -0600

    Update runRHAnalyzer.py

--- runRHAnalyzer.py
+++ runRHAnalyzer.py
@@ -9,7 +9,7 @@ inputFiles_='file:/uscms/home/ecentamo/nobackup/CMSSW_13_0_13/src/MCProduction/E
 maxEvents_=-1
 skipEvents_=0#
 # outputFile_='MLAnalyzer_HToAATo2Tau2Photon_with_triggers.root'
-outputFile_='MLAnalyzer_HToAATo2Gluon2Photon.root'
+outputFile_='MLAnalyzer_HToAATo2Gluon2Photon_11_17_2.root'
 
 # cmd="cmsTraceExceptions cmsRun %s inputFiles=%s maxEvents=%d skipEvents=%d outputFile=%s"%(cfg,inputFiles_,maxEvents_,skipEvents_,outputFile_)
 cmd="cmsRun %s inputFiles=%s maxEvents=%d skipEvents=%d outputFile=%s"%(cfg,inputFiles_,maxEvents_,skipEvents_,outputFile_)
